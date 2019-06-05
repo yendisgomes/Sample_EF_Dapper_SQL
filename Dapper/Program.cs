5c1782850684ac3e86c1bca4e03ab1bc7f4cf778
Yendis dos Santos Gomes <yendis.gomes@hotmail.com>
2019-06-05 00:48:58 -0300

    Add project files.

---
+++ Dapper/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Dapper
+{
+    class Program
+    {
+
+        static string connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            DTO();
+        }
+
+
+        private static void DTO()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                IEnumerable pessoas = connection.Query<Pessoa>("Select * from Pessoa");
+                Console.WriteLine("{0} - {1} - {2} - {3} - {4}", "Id", "Nome", "Telefone", "Endereço","Cidade","Estado");
+                foreach (Pessoa pessoa in pessoas)
+                {
+                    Console.WriteLine("{0} - {1} - {2} - {3} - {4}", pessoa.Id, pessoa.Nome, pessoa.Telefone, pessoa.Endereco, pessoa.Cidade, pessoa.Estado);
+                }
+                connection.Close();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ SQL/Program.cs
@@ -0,0 +1,74 @@
+﻿using Sample_SQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_SQL
+{
+    class Program
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["conexaoNorthwind"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            var produtos = GetList();
+
+            Console.WriteLine("{0} - {1} - {2} - {3}", "Id", "ProductName", "QuantityPerUnit", "UnitPrice");
+            foreach (var produto in produtos)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}", 
+                    produto.ProductId, produto.ProductName, produto.QuantityPerUnit, produto.UnitPrice);    
+            }
+            Console.ReadLine();
+        }
+
+        static List<Produto> GetList()
+        {
+            List<Produto> Produtos = new List<Produto>();
+            Produto produto = null;
+            
+            string sqlQuery = "SELECT * FROM Products";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {    //Every new row will create a new dictionary that holds the columns
+                    produto = new Produto();
+
+                    produto.ProductId = Convert.ToInt32(reader["ProductId"]);
+                    produto.ProductName = reader["ProductName"].ToString();
+                    produto.SupplierID = Convert.ToInt32(reader["SupplierID"]);
+                    produto.CategoryID = Convert.ToInt32(reader["CategoryID"]);
+                    produto.QuantityPerUnit = reader["QuantityPerUnit"].ToString();
+                    produto.UnitPrice = Convert.ToDecimal(reader["UnitPrice"]);
+
+                    Produtos.Add(produto);
+                }
+                
+                reader.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return Produtos;
+        }
+    }
+}
